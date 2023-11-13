b4ee8b1bbb3e7c429af12f10d458c488aab2e8c2
niels <dev@nielsdekker.nl>
2023-11-13 22:19:55 +0100

    Small tweaks to nvim

--- nvim/lua/plugins/mini_completion.lua
+++ nvim/lua/plugins/mini_completion.lua
@@ -4,6 +4,22 @@ return {
     "echasnovski/mini.nvim",
   },
   config = function()
-    require("mini.completion").setup({})
+    require("mini.completion").setup({
+      -- Fallback to files
+      fallback_action = "<C-x><C-f>",
+      lsp_completion = {
+        process_items = function(items)
+          local completionKind = vim.lsp.protocol.CompletionItemKind;
+          local res = vim.tbl_filter(function(item)
+            -- Remove text and snippet kinds
+            return item.kind ~= completionKind.Text and item.kind ~= completionKind.Snippet
+          end, items)
+
+          table.sort(res, function(a, b) return (a.sortText or a.label) < (b.sortText or b.label) end)
+
+          return res
+        end
+      }
+    })
   end
 }
