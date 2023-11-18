86922ba5e9116215b6910ac7f27f764ff37a9eb8
niels <dev@nielsdekker.nl>
2023-11-18 14:23:34 +0100

    Added C-q command to send picked items to quickfix

--- nvim/lua/plugins/mini_pick.lua
+++ nvim/lua/plugins/mini_pick.lua
@@ -5,10 +5,25 @@ return {
     "echasnovski/mini.nvim",
   },
   config = function()
-    require("mini.pick").setup({
-      window = {
-        config = {
-          border = "rounded"
+    local pick = require("mini.pick")
+    pick.setup({
+      mappings = {
+        to_quick_fix = {
+          char = "<C-q>",
+          func = function()
+            if (pick.is_picker_active() == false) then
+              return;
+            end
+
+            -- Get all the matched items and send them to the quickfix list.
+            -- Also make sure the picker is closed and the quickfix list
+            -- focussed.
+            pick.default_choose_marked(
+              pick.get_picker_matches().all
+            )
+            pick.stop()
+            vim.cmd("copen")
+          end
         }
       }
     })
