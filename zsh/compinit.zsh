673e6be6f0c2219c8fa9ae8e135871a00873ca7b
Niels Dekker <niels@hubper.co>
2024-01-16 10:30:40 +0100

    Added case insensitive completion

--- zsh/compinit.zsh
+++ zsh/compinit.zsh
@@ -1,3 +1,7 @@
 zstyle :compinstall filename '~/.zshrc'
 autoload -Uz compinit && compinit
+
+# Makes completion case insensitive
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
 source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
