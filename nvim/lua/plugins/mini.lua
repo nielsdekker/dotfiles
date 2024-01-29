156c138aab7d8d018f04d9435a0ffba711432d60
niels <dev@nielsdekker.nl>
2024-01-29 20:28:09 +0100

    Allowed items to be removed from mini visits and other pickers
    This commit also has a small fix for a small typescript annoyance

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -1,18 +1,33 @@
 local function setup_mini_completion()
-  require("mini.completion").setup({
+  local completion = require("mini.completion")
+
+  completion.setup({
     -- Fallback to files
     fallback_action = "<C-x><C-f>",
     lsp_completion = {
-      process_items = function(items)
+      process_items = function(items, base)
         local completionKind = vim.lsp.protocol.CompletionItemKind;
-        local res = vim.tbl_filter(function(item)
+        local result = vim.tbl_filter(function(item)
           -- Remove text and snippet kinds
           return item.kind ~= completionKind.Text and item.kind ~= completionKind.Snippet
         end, items)
 
-        table.sort(res, function(a, b) return (a.sortText or a.label) < (b.sortText or b.label) end)
+        -- Make sure the list is sorted
+        table.sort(result, function(a, b)
+          return (a.sortText or a.label) < (b.sortText or b.label)
+        end)
 
-        return res
+        -- Fix for specific issue with typescript completion adding extra dots
+        -- for some reason.
+        -- https://github.com/echasnovski/mini.nvim/issues/306
+        for _, item in ipairs(result) do
+          local new_text = (item.textEdit or {}).newText
+          if type(new_text) == 'string' then
+            item.textEdit.newText = new_text:gsub("^%.+", '')
+          end
+        end
+
+        return completion.default_process_items(result, base)
       end
     }
   })
@@ -34,27 +49,53 @@ end
 
 local function setup_mini_pick()
   local pick = require("mini.pick")
+  local visits = require("mini.visits")
+
   pick.setup({
     mappings = {
       move_down = "<C-j>",
       move_up = "<C-k>",
-      to_quick_fix = {
+
+      -- Custom mapping that sends all visible matches to the quickfix list and
+      -- then opens it.
+      quick_fix_add_all = {
         char = "<C-q>",
         func = function()
           if (pick.is_picker_active() == false) then
             return;
           end
 
-          -- Get all the matched items and send them to the quickfix list.
-          -- Also make sure the picker is closed and the quickfix list
-          -- focussed.
           pick.default_choose_marked(
             pick.get_picker_matches().all
           )
           pick.stop()
           vim.cmd("copen")
         end
-      }
+      },
+
+      -- This custom thingy allows you to press C-d to delete the currently
+      -- highlighted item from the matches view. When this is the "visits"
+      -- picker it will also remove the item from the visits list.
+      mini_pick_delete = {
+        char = '<C-d>',
+        func = function()
+          if (pick.is_picker_active() == false) then
+            return;
+          end
+
+          local opts_source = pick.get_picker_opts().source
+          local matches = pick.get_picker_matches()
+
+          -- For the visits picker we also remove the match from the state
+          if (string.find(opts_source.name, "Visit") == 1) then
+            visits.remove_path(matches.current, opts_source.cwd)
+          end
+
+          -- Update the active picker
+          table.remove(matches.all, matches.current_ind)
+          pick.set_picker_items(matches.all)
+        end
+      },
     }
   })
 end
