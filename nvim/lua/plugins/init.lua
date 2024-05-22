bed2d96a1af1ef503411d2eb4ac8ea71700ccdf3
Niels Dekker <niels@hubper.co>
2024-05-22 13:18:22 +0200

    Fix the highlight of inlay elements

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -50,4 +50,8 @@ now(function()
   })
 
   vim.cmd("colorscheme tokyonight")
+
+  -- Fix the inlay hint styling
+  local current = vim.api.nvim_get_hl(0, { name = "LspInlayHint" })
+  vim.api.nvim_set_hl(0, "LspInlayHint", { bg = nil, fg = current.fg, italic = true })
 end)
