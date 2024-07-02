bbc9216da562ee543113bf02932fb53d645bd9e8
Niels Dekker <dev@nielsdekker.nl>
2024-07-02 20:46:42 +0200

    And removed unused variables

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -89,8 +89,6 @@ local function setup_mini_statusline()
     content = {
       active = function()
         local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-        local git           = MiniStatusline.section_git({ trunc_width = 40 })
-        local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
         local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
         local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
         local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
