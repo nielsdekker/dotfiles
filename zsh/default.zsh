79cf40aaf53d836b2d9bbfb5bd65bee2adad72a7
Niels Dekker <niels@hubper.co>
2024-07-19 13:20:40 +0200

    Add background jobs to the PS1

--- zsh/default.zsh
+++ zsh/default.zsh
@@ -8,7 +8,7 @@ precmd_functions+=( precmd_vcs_info )
 setopt PROMPT_SUBST
 zstyle ':vcs_info:git:*' formats '[%b]'
 
-PROMPT=$'%{$reset_color%}\n%{%F{cyan}%}%~ %{%F{red}%}${vcs_info_msg_0_}'$'\n''$ %{$reset_color%}'
+PROMPT=$'%{$reset_color%}\n%{%F{yellow}%}%(1j.[%j] .)%{%F{cyan}%}%~ %{%F{red}%}${vcs_info_msg_0_}'$'\n''$ %{$reset_color%}'
 RPROMPT='%{%F{white}%}%*'
 
 #####
