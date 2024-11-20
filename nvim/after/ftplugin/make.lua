47d0fdd62f8a81bda36d2526b80578d706a45b5f
Niels Dekker <niels@hubper.co>
2024-11-20 16:56:25 +0100

    Small fix for conflict with ftplugin and editorconfig

--- nvim/after/ftplugin/make.lua
+++ nvim/after/ftplugin/make.lua
@@ -1,2 +1,6 @@
 -- Use actual tabs in make files. Tabs have a special meaning in these files.
 vim.o.expandtab = false
+
+-- Disable the editor config for this. Tabs are required in makefiles and we
+-- don't want the editor config to enforce spaces instead.
+vim.b.editorconfig = false
