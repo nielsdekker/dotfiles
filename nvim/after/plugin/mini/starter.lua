b65a6d09cb408d4a6179c6a730363305e40dfb91
Niels Dekker <dev@nielsdekker.nl>
2025-05-07 20:10:27 +0200

    Customized the mini.starter header

--- nvim/after/plugin/mini/starter.lua
+++ nvim/after/plugin/mini/starter.lua
@@ -1,3 +1,28 @@
+local ascii = {
+    {
+        "    |\\__/,|   (`\\ ",
+        "  _.|o o  |_   ) )",
+        "-(((---(((--------",
+    },
+    {
+        ' _._     _,-\'""`-._    ',
+        "(,-.`._,'(       |\\`-/|",
+        "    `-.-' \\ )-`( , o o)",
+        "          `-    \\`_`\"'-",
+    },
+    {
+        " |\\__/,|   (`\\ ",
+        " |_ _  |.--.) )",
+        " ( T   )     / ",
+        "(((^_(((/(((_/ ",
+    },
+}
+
 require("mini.deps").now(function()
-    require("mini.starter").setup()
+    math.randomseed(os.time())
+    local i = math.random(1, #ascii)
+
+    require("mini.starter").setup({
+        header = table.concat(ascii[i], "\n")
+    })
 end)
