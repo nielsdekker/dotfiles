116bce1db8b77fe0e2de9d8be94cff581e7daf22
Niels Dekker <dev@nielsdekker.nl>
2025-12-08 19:20:28 +0100

    Use a module for the statusline

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,4 +1,101 @@
-local icons = {
+local S = {}
+
+StatusLine = function()
+    return table.concat({
+        S.get_mode(),
+        S.get_diagnostics(),
+        S.get_git_branch_name(),
+        S.with_hl(" %f", S.groups.default),
+        -- Moves to the end
+        "%=",
+        S.get_cursor_info()
+    })
+end
+
+vim.o.statusline = '%!v:lua.StatusLine()'
+
+--- Creates a mode block to show whether or not we are in insert mode
+S.get_mode = function()
+    local modes = {
+        ["n"] = { text = "Normal", hl = S.groups.mode_normal },
+        ["v"] = { text = "Visual", hl = S.groups.mode_visual },
+        ["V"] = { text = "Visual", hl = S.groups.mode_visual },
+        -- Matches ^V
+        ["\22"] = { text = "Visual", hl = S.groups.mode_visual },
+        ["s"] = { text = "Select", hl = S.groups.mode_select },
+        ["S"] = { text = "Select", hl = S.groups.mode_select },
+        -- Matches ^S
+        ["\19"] = { text = "Select", hl = S.groups.mode_select },
+        ["i"] = { text = "Insert", hl = S.groups.mode_insert },
+        ["R"] = { text = "Replace", hl = S.groups.mode },
+        ["c"] = { text = "Command", hl = S.groups.mode },
+        ["r"] = { text = "Prompt", hl = S.groups.mode },
+        ["!"] = { text = "Shell", hl = S.groups.mode },
+        ["t"] = { text = "Terminal", hl = S.groups.mode },
+    }
+
+    local mode = string.sub(vim.api.nvim_get_mode().mode, 1, 2)
+    local m = modes[mode]
+
+    if m ~= nil then
+        return S.with_border(m.text, m.hl)
+    else
+        return S.with_border(mode, S.groups.mode)
+    end
+end
+
+--- Returns the git branch
+S.get_git_branch_name = function()
+    local branch_name = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+
+    if branch_name ~= "" then
+        return S.with_hl(S.icons.git .. " " .. branch_name, S.groups.git)
+    else
+        return ""
+    end
+end
+
+--- Diagnostics from the LSP and other thingies that set diagnostics
+S.get_diagnostics = function()
+    local error_count = vim.tbl_count(vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR }))
+    local warning_count = vim.tbl_count(vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN }))
+
+    if error_count ~= 0 and warning_count ~= 0 then
+        return " "
+            .. S.with_hl(S.icons.border_left, S.groups.diag_errors .. "Inverse")
+            .. S.with_hl(S.icons.err .. " " .. error_count, S.groups.diag_errors)
+            .. S.with_hl(S.icons.triangle, S.groups.diag_seperator)
+            .. S.with_hl(S.icons.warn .. " " .. warning_count, S.groups.diag_warnings)
+            .. S.with_hl(S.icons.border_right, S.groups.diag_warnings .. "Inverse")
+            .. " "
+    elseif error_count ~= 0 then
+        return S.with_border(S.icons.err .. " " .. error_count, S.groups.diag_errors)
+    elseif warning_count ~= 0 then
+        return S.with_border(S.icons.warn .. " " .. warning_count, S.groups.diag_warnings)
+    else
+        return ""
+    end
+end
+
+S.get_cursor_info = function()
+    return S.with_border("[l:%l c:%c]", S.groups.info)
+end
+
+-- Renders the given string with the highlight
+S.with_hl = function(s, hl)
+    return "%#" .. hl .. "#" .. s
+end
+
+-- Renders the string and add borders to the left and right of it.
+S.with_border = function(s, hl)
+    return " "
+        .. S.with_hl(S.icons.border_left, hl .. "Inverse")
+        .. S.with_hl(s, hl)
+        .. S.with_hl(S.icons.border_right, hl .. "Inverse")
+        .. " "
+end
+
+S.icons = {
     git = "",
     err = "",
     warn = "",
@@ -7,7 +104,7 @@ local icons = {
     triangle = "",
 }
 
-local groups = {
+S.groups = {
     -- Small groups
     default = "StatusLine",
     git = "StatusLineGit",
@@ -25,98 +122,3 @@ local groups = {
     diag_warnings = "StatusLineDiagnosticWarnings",
     diag_seperator = "StatusLineDiagnosticSeparator",
 }
-
--- Renders the given string with the highlight
-local function with_hl(s, hl)
-    return "%#" .. hl .. "#" .. s
-end
-
--- Renders the string and add borders to the left and right of it.
-local function with_border(s, hl)
-    return " "
-        .. with_hl(icons.border_left, hl .. "Inverse")
-        .. with_hl(s, hl)
-        .. with_hl(icons.border_right, hl .. "Inverse")
-        .. " "
-end
-
---- Creates a mode block to show whether or not we are in insert mode
-local function get_mode()
-    local modes = {
-        ["n"] = { text = "Normal", hl = groups.mode_normal },
-        ["v"] = { text = "Visual", hl = groups.mode_visual },
-        ["V"] = { text = "Visual", hl = groups.mode_visual },
-        -- Matches ^V
-        ["\22"] = { text = "Visual", hl = groups.mode_visual },
-        ["s"] = { text = "Select", hl = groups.mode_select },
-        ["S"] = { text = "Select", hl = groups.mode_select },
-        -- Matches ^S
-        ["\19"] = { text = "Select", hl = groups.mode_select },
-        ["i"] = { text = "Insert", hl = groups.mode_insert },
-        ["R"] = { text = "Replace", hl = groups.mode },
-        ["c"] = { text = "Command", hl = groups.mode },
-        ["r"] = { text = "Prompt", hl = groups.mode },
-        ["!"] = { text = "Shell", hl = groups.mode },
-        ["t"] = { text = "Terminal", hl = groups.mode },
-    }
-
-    local mode = string.sub(vim.api.nvim_get_mode().mode, 1, 2)
-    local m = modes[mode]
-
-    if m ~= nil then
-        return with_border(m.text, m.hl)
-    else
-        return with_border(mode, groups.mode)
-    end
-end
-
---- Returns the git branch
-local function get_git_branch_name()
-    local branch_name = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
-
-    if branch_name ~= "" then
-        return with_hl(icons.git .. " " .. branch_name, groups.git)
-    else
-        return ""
-    end
-end
-
---- Diagnostics from the LSP and other thingies that set diagnostics
-local function get_diagnostics()
-    local error_count = vim.tbl_count(vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR }))
-    local warning_count = vim.tbl_count(vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN }))
-
-    if error_count ~= 0 and warning_count ~= 0 then
-        return " "
-            .. with_hl(icons.border_left, groups.diag_errors .. "Inverse")
-            .. with_hl(icons.err .. " " .. error_count, groups.diag_errors)
-            .. with_hl(icons.triangle, groups.diag_seperator)
-            .. with_hl(icons.warn .. " " .. warning_count, groups.diag_warnings)
-            .. with_hl(icons.border_right, groups.diag_warnings .. "Inverse")
-            .. " "
-    elseif error_count ~= 0 then
-        return with_border(icons.err .. " " .. error_count, groups.diag_errors)
-    elseif warning_count ~= 0 then
-        return with_border(icons.warn .. " " .. warning_count, groups.diag_warnings)
-    else
-        return ""
-    end
-end
-
-local function get_cursor_info()
-    return with_border("[l:%l c:%c]", groups.info)
-end
-
-StatusLine = function()
-    return table.concat({
-        get_mode(),
-        get_diagnostics(),
-        get_git_branch_name(),
-        with_hl(" %f", groups.default),
-        -- Moves to the end
-        "%=",
-        get_cursor_info()
-    })
-end
-
-vim.o.statusline = '%!v:lua.StatusLine()'
