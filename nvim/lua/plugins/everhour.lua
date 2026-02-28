66469c61072024566776bd446a8924be47b0aa46
Niels Dekker <dev@nielsdekker.nl>
2026-02-28 16:05:55 +0100

    And made sure the everhour plugin works

--- nvim/lua/plugins/everhour.lua
+++ nvim/lua/plugins/everhour.lua
@@ -1,19 +1,16 @@
---- @type MiniDep
-return {
-	source = "git@github.com:hubper/devday-everhour.nvim.git",
-	name = "everhour",
-	setup = function()
-		local token = os.getenv("EVERHOUR_TOKEN")
-		local pattern = os.getenv("EVERHOUR_PATTERN")
+vim.pack.add({
+	{ src = "git@github.com:hubper/devday-everhour.nvim.git", name = "everhour" },
+})
 
-		if token == nil or pattern == nil then
-			-- No settings for everhour so no need to start it
-			return
-		end
+local token = os.getenv("EVERHOUR_TOKEN")
+local pattern = os.getenv("EVERHOUR_PATTERN")
 
-		require("everhour").setup({
-			pattern = pattern,
-			token = token,
-		})
-	end,
-}
+if token == nil or pattern == nil then
+	-- No settings for everhour so no need to start it
+	return
+end
+
+require("everhour").setup({
+	pattern = pattern,
+	token = token,
+})
