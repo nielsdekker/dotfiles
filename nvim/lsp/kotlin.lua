0dff87290fc1ed1c2c8f091621d5396da6c098f1
Niels Dekker <dev@nielsdekker.nl>
2026-04-28 16:23:01 +0200

    Updated kotlin lsp

--- nvim/lsp/kotlin.lua
+++ nvim/lsp/kotlin.lua
@@ -1,11 +1,8 @@
 ---@type vim.lsp.Config
 return {
 	name = "kotlin-lsp",
-	cmd = { "kotlin-lsp", "--stdio" },
+	cmd = { "intellij-server", "--stdio" },
 	single_file_support = false,
 	filetypes = { "kotlin" },
 	root_markers = { ".git" },
-	cmd_env = {
-		JAVA_OPTS = "-Xms1024m -Xmx8192m",
-	},
 }
