339dc03ec27430f209cd3c3f952226ba7a150d81
Niels Dekker <dev@nielsdekker.nl>
2026-05-22 09:44:04 +0200

    Mise minimum age and removed kotlin-lsp

--- nvim/lsp/kotlin.lua
+++
@@ -1,8 +0,0 @@
----@type vim.lsp.Config
-return {
-	name = "kotlin-lsp",
-	cmd = { "intellij-server", "--stdio" },
-	single_file_support = false,
-	filetypes = { "kotlin" },
-	root_markers = { ".git" },
-}
